9b246cbd57f8cdafb68e247d481785374438a0ee
yassersalem74 <yassersalem9099@gmail.com>
2026-02-09 20:29:49 +0200

    Feature/ change logo app

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/Home-Page/HeroSection";
+import AboutSection from "../components/Home-Page/AboutSection";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection></HeroSection>
+      <AboutSection></AboutSection>
     </div>
   );
 }
