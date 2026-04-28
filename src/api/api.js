8c55c98e375ad5cd2b9c2f6554165857e0394ad7
yassersalem74 <yassersalem9099@gmail.com>
2026-04-28 13:39:21 +0300

    FIX CORS

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "https://alaaeldin.runasp.net",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default api;
